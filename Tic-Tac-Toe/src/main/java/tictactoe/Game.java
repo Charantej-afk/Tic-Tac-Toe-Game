0cb8c0596613f266bc26242d0f1db9cb7a35c6bd
Charantej Akri <b.charantej@ajacs.in>
2025-10-08 15:47:14 +0530

    Update Game.java

--- Tic-Tac-Toe/src/main/java/tictactoe/Game.java
+++ Tic-Tac-Toe/src/main/java/tictactoe/Game.java
@@ -1,57 +1,98 @@
-package tictactoe;
+package mypackage;
 
-public class Game {
-    private final char[][] board = new char[3][3];
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet("/play")
+public class Game extends HttpServlet {
+
+    private char[][] board = new char[3][3];
     private char currentPlayer = 'X';
-    private Character winner = null;
-    private int moves = 0;
 
-    public Game() {
-        for (int i = 0; i < 3; i++)
-            for (int j = 0; j < 3; j++)
-                board[i][j] = ' ';
+    @Override
+    public void init() throws ServletException {
+        resetBoard();
     }
 
-    public boolean playMove(int row, int col) {
-        if (winner != null || board[row][col] != ' ') return false;
-        board[row][col] = currentPlayer;
-        moves++;
-        if (checkWin(row, col)) {
-            winner = currentPlayer;
-        } else if (moves < 9) {
-            currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+    private void resetBoard() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = '-';
+            }
         }
+        currentPlayer = 'X';
+    }
+
+    private boolean makeMove(int row, int col) {
+        if (row >= 0 && row < 3 && col >= 0 && col < 3 && board[row][col] == '-') {
+            board[row][col] = currentPlayer;
+            return true;
+        }
+        return false;
+    }
+
+    private boolean checkWin() {
+        // rows
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0] != '-' && board[i][0] == board[i][1] && board[i][1] == board[i][2])
+                return true;
+        }
+        // cols
+        for (int i = 0; i < 3; i++) {
+            if (board[0][i] != '-' && board[0][i] == board[1][i] && board[1][i] == board[2][i])
+                return true;
+        }
+        // diagonals
+        if (board[0][0] != '-' && board[0][0] == board[1][1] && board[1][1] == board[2][2])
+            return true;
+        if (board[0][2] != '-' && board[0][2] == board[1][1] && board[1][1] == board[2][0])
+            return true;
+        return false;
+    }
+
+    private boolean isBoardFull() {
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
+                if (board[i][j] == '-')
+                    return false;
         return true;
     }
 
-    public String getBoardHtml() {
-        StringBuilder sb = new StringBuilder("<table border='1' style='font-size:2em'>");
-        for (int i = 0; i < 3; i++) {
-            sb.append("<tr>");
-            for (int j = 0; j < 3; j++) {
-                sb.append("<td style='width:40px;height:40px;text-align:center;'>")
-                  .append(board[i][j])
-                  .append("</td>");
+    private void switchPlayer() {
+        currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        String moveRow = request.getParameter("row");
+        String moveCol = request.getParameter("col");
+
+        String message = "";
+
+        if (moveRow != null && moveCol != null) {
+            int row = Integer.parseInt(moveRow);
+            int col = Integer.parseInt(moveCol);
+            if (makeMove(row, col)) {
+                if (checkWin()) {
+                    message = "Player " + currentPlayer + " wins! 🎉";
+                } else if (isBoardFull()) {
+                    message = "It's a draw!";
+                } else {
+                    switchPlayer();
+                }
+            } else {
+                message = "Invalid move! Try again.";
             }
-            sb.append("</tr>");
         }
-        sb.append("</table>");
-        return sb.toString();
-    }
 
-    public Character getWinner() {
-        return winner;
-    }
-
-    public boolean isDraw() {
-        return moves == 9 && winner == null;
-    }
-
-    private boolean checkWin(int row, int col) {
-        char p = currentPlayer;
-        return (board[row][0] == p && board[row][1] == p && board[row][2] == p) ||
-               (board[0][col] == p && board[1][col] == p && board[2][col] == p) ||
-               (row == col && board[0][0] == p && board[1][1] == p && board[2][2] == p) ||
-               (row + col == 2 && board[0][2] == p && board[1][1] == p && board[2][0] == p);
-    }
-}
+        out.println("<html><head><title>Tic Tac Toe</title></head>");
+        out.println("<body style='text-align:center; font-family:Arial;'>");
+        out.println("<h1>Tic Tac Toe Game 🎮</h1>");
+        out
